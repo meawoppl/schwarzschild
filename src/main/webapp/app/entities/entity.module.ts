7f63eddfbf218835002ec388edfb0b5b41dc4b2b
meawoppl <meawoppl@gmail.com>
2018-04-22 14:34:34 -0700

    Created linkage

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { SchwarzschildStoneModule } from './stone/stone.module';
+import { SchwarzschildLinkageModule } from './linkage/linkage.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        SchwarzschildStoneModule,
+        SchwarzschildLinkageModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
