588aa526da44209daa38346ce545e9c69dfac693
meawoppl <meawoppl@gmail.com>
2018-04-22 09:44:47 -0700

    jhipster init files

---
+++ src/main/java/net/mattyg/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package net.mattyg.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Schwarzschild.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
---
+++ src/main/java/net/mattyg/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package net.mattyg.config.audit;
